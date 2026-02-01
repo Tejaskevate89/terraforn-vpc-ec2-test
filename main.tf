7b79f2b63e9c1ef054b646ffcc18bcf192a2df4d
TEJAS <tejaskevate89@gmail.com>
2026-02-01 19:34:22 +0530

    module creat

--- main.tf
+++ main.tf
@@ -25,5 +25,5 @@ module "my_instance" {
   instance_type = var.instance_type
   key_name = var.key_name
   sgid = var.sgid
-  subnet_id = module.my_vpc.aws_subnet.my_subnet.id
+  #subnet_id = module.my_vpc.aws_subnet.my_subnet.id
 }
